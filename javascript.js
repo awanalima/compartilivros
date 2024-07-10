0066d8471150f395249dd24d01d5d02ca8e56470
Awana Lima <147575253+awanalima@users.noreply.github.com>
2024-07-10 18:36:15 -0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-/*window.alert("Antes de iniciar sua inscrição, gostaríamos de te conhecer um pouquinho!")
-var nome = window.prompt ("Qual seu nome?")
-var livro = window.prompt (nome + ", qual seu livro favorito?")
-window.alert("Obrigado por compartilhar conosco, " + nome +"! Boa leitura!")
-window.alert("É um prazer te conhecer, " + nome + "! Volte sempre!") */
 
 window.addEventListener('scroll', function(){
     let header = document.querySelector('#header')
